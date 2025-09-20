e227b4a1f802a0b57f634954260aff391c77ba83
Antim Vacation <antimgurung314@gmail.com>
2025-09-20 22:07:48 +0545

    message

---
+++ 35-36 Contecxt API/src/App.jsx
@@ -0,0 +1,62 @@
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import TodoItems from "./components/TodoItems";
+import WelcomeMessage from "./components/WelcomeMessage";
+import './App.css';
+import { useState } from "react";
+
+
+
+function App() {
+
+  // const initialTodoItems = [
+  //   {
+  //     name: "Have to study",
+  //     dueDate: "2025/10/4"
+  //   },
+     
+  //   {
+  //     name: "Go to trekking",
+  //     dueDate: "2024/8/6"
+  //   },
+
+  //   {
+  //     name: "Watch a movie",
+  //     dueDate: "2024/8/11"
+  //   }
+ //
+
+  const [todoItems,setTodoItems] = useState([]);
+
+  const handleNewItems = (itemName, itemDueDate) => {
+  // console.log(`New Item Added: ${itemName} Date: ${itemDueDate}`);
+  const newTodoItems = [...todoItems,{name: itemName, dueDate: itemDueDate}];
+  setTodoItems(newTodoItems);
+};
+
+//  const handleNewItems = (itemName, itemDueDate) => {
+//   const newTodoItems = [...todoItems,{name: itemName, dueDate: itemDueDate}];
+//   setTodoItems((currValue)=> 
+//     [...currValue,{name: itemName, dueDate: itemDueDate}]);
+//   return newTodoItems;
+// };
+
+
+
+  const handleDeleteItem = (todoItemName) =>{
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName)
+    setTodoItems(newTodoItems);
+    
+  }
+
+
+  return <center
+    className="todo-container">
+    <AppName />
+    <AddTodo onNewItem= {handleNewItems}/>
+    {todoItems.length === 0 && <WelcomeMessage />}
+    <TodoItems todoItems={todoItems} onDeleteClick ={handleDeleteItem}></TodoItems>
+  </center>
+
+};
+export default App;
---
+++ 35-36 Contecxt API/src/components/AddTodo.jsx
@@ -0,0 +1,65 @@
+import { useState, useRef} from "react";
+import { RiAddLargeFill } from "react-icons/ri";
+
+
+function AddTodo({onNewItem}) {
+
+  const[todoName,setTodoName] = useState("");
+  const [dueDate,setdueDate] = useState("");
+  const noOfClicks = useRef(0);
+  // const todoNameElement = useRef();
+  // const dueDateElement = useRef();
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+    noOfClicks.current = noOfClicks.current + 1;
+    console.log(noOfClicks.current);
+  };
+
+  const handleDateChange = (event) => {
+    setdueDate(event.target.value);
+  }
+
+
+   const handleAddButtonClicked = (event) => {
+    // event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    // todoNameElement.current.value = "";
+    // dueDateElement.current.value = "";
+    onNewItem(todoName,dueDate);
+    setTodoName("");
+    setdueDate("");
+   
+   }
+
+  return(
+    <div className="container">
+
+    <form className=" row kg-row" 
+    // onSubmit={handleAddButtonClicked}
+    >
+      
+    <div className="col-6" ><input type="text" placeholder="Enter todo here" 
+    value ={todoNameElement}
+    onChange={handleNameChange}
+    /></div>
+
+    <div className="col-4" 
+    value={dueDate}
+    ref = {dueDateElement}
+    onChange={handleDateChange}
+    ><input type="date"/></div>
+
+
+    <div className="col-2">
+    <button type="submit" className="btn btn-success kg-button"
+    onClick ={handleAddButtonClicked}
+    >
+      <RiAddLargeFill /></button>
+   </div>
+   </form>
+   </div>
+  );
+};
+export default AddTodo;
